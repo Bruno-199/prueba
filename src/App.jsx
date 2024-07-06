812f476e655a1ab207e31d03655b5edb1d30ec5e
bruno <bazanbruno199@gmail.com>
2024-07-06 11:32:57 -0300

    probando

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Portoflio Bruno Bazan</h1>
+      <h1>Portoflio Bruno Bazan y Monica</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
